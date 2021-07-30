2cb38ec94d1b4acd8d232c8062673abf9d0a9020
Elves Sousa <elves.sousa@gmail.com>
2021-07-30 18:39:06 -0300

    Chapter IV: Data fetching and local cache

--- src/components/Pagination/PaginationItem.tsx
+++ src/components/Pagination/PaginationItem.tsx
@@ -3,9 +3,14 @@ import { Button } from '@chakra-ui/react';
 type PaginationItemProps = {
   number: number;
   isCurrent?: boolean;
+  onPageChange: (page: number) => void;
 };
 
-export function PaginationItem({ isCurrent, number }: PaginationItemProps) {
+export function PaginationItem({
+  isCurrent,
+  number,
+  onPageChange,
+}: PaginationItemProps) {
   if (isCurrent) {
     return (
       <Button
@@ -30,6 +35,7 @@ export function PaginationItem({ isCurrent, number }: PaginationItemProps) {
       _hover={{
         bg: 'gray.500',
       }}
+      onClick={() => onPageChange(number)}
     >
       {number}
     </Button>
--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -6,6 +6,8 @@ import {
   Flex,
   Heading,
   Icon,
+  Link as ChakraLink,
+  Spinner,
   Table,
   Tbody,
   Td,
@@ -20,12 +22,41 @@ import { RiAddLine, RiPencilLine } from 'react-icons/ri';
 import { Header } from '../../components/Header';
 import { Pagination } from '../../components/Pagination';
 import { Sidebar } from '../../components/Sidebar';
+import { getUsers, useUsers, User } from '../../services/hooks/useUsers';
+import { useState } from 'react';
+import { queryClient } from '../../services/queryClient';
+import { api } from '../../services/api';
+import { GetServerSideProps } from 'next';
 
-export default function UserList() {
+type UserListProps = {
+  users: User[];
+};
+
+export default function UserList({ users }: UserListProps) {
+  const [page, setPage] = useState(1);
+  const { data, isLoading, isFetching, error } = useUsers(page, {
+    initialData: users,
+  });
+
   const isWideVersion = useBreakpointValue({
     base: false,
     lg: true,
   });
+
+  async function handlePrefetchUser(userId: string) {
+    await queryClient.prefetchQuery(
+      ['user', userId],
+      async () => {
+        const response = await api.get(`users/${userId}`);
+
+        return response.data;
+      },
+      {
+        staleTime: 1000 * 60 * 10, // 10 minutes
+      }
+    );
+  }
+
   return (
     <Box>
       <Header />
@@ -37,6 +68,9 @@ export default function UserList() {
           <Flex mb="8" justify="space-between" align="center">
             <Heading size="lg" fontWeight="normal">
               Usuários
+              {!isLoading && isFetching && (
+                <Spinner size="sm" color="gray.500" ml="4" />
+              )}
             </Heading>
             <Link href="/users/create" passHref>
               <Button
@@ -51,51 +85,84 @@ export default function UserList() {
             </Link>
           </Flex>
 
-          <Table colorScheme="whiteAlpha">
-            <Thead>
-              <Tr>
-                <Th px={['4', '4', '6']} color="gray.300" width="8">
-                  <Checkbox colorScheme="orange" />
-                </Th>
-                <Th>Usuário</Th>
-                {isWideVersion && <Th>Data de cadastro</Th>}
-                {isWideVersion && <Th w="8"></Th>}
-              </Tr>
-            </Thead>
-            <Tbody>
-              <Tr>
-                <Td px={['4', '4', '6']}>
-                  <Checkbox colorScheme="orange" />
-                </Td>
-                <Td>
-                  <Box>
-                    <Text fontWeight="bold">Elves Sousa</Text>
-                    <Text fontWeight="sm" color="gray.300">
-                      elvessousa@icloud.com
-                    </Text>
-                  </Box>
-                </Td>
-                {isWideVersion && <Td>31 de Junho, 2021</Td>}
-                {isWideVersion && (
-                  <Td>
-                    <Button
-                      as="a"
-                      size="sm"
-                      fontSize="sm"
-                      colorScheme="teal"
-                      leftIcon={<Icon as={RiPencilLine} />}
-                    >
-                      Editar
-                    </Button>
-                  </Td>
-                )}
-              </Tr>
-            </Tbody>
-          </Table>
+          {isLoading ? (
+            <Flex justify="center">
+              <Spinner />
+            </Flex>
+          ) : error ? (
+            <Flex justify="center">
+              <Text>Falha ao obter dados dos usuários.</Text>
+            </Flex>
+          ) : (
+            <>
+              <Table colorScheme="whiteAlpha">
+                <Thead>
+                  <Tr>
+                    <Th px={['4', '4', '6']} color="gray.300" width="8">
+                      <Checkbox colorScheme="orange" />
+                    </Th>
+                    <Th>Usuário</Th>
+                    {isWideVersion && <Th>Data de cadastro</Th>}
+                    {isWideVersion && <Th w="8"></Th>}
+                  </Tr>
+                </Thead>
+                <Tbody>
+                  {data.users.map((user) => (
+                    <Tr key={user.id}>
+                      <Td px={['4', '4', '6']}>
+                        <Checkbox colorScheme="orange" />
+                      </Td>
+                      <Td>
+                        <Box>
+                          <ChakraLink
+                            color="orange.500"
+                            onMouseEnter={() => handlePrefetchUser(user.id)}
+                          >
+                            <Text fontWeight="bold">{user.name}</Text>
+                          </ChakraLink>
+                          <Text fontWeight="sm" color="gray.300">
+                            {user.email}
+                          </Text>
+                        </Box>
+                      </Td>
+                      {isWideVersion && <Td>{user.createdAt}</Td>}
+                      {isWideVersion && (
+                        <Td>
+                          <Button
+                            as="a"
+                            size="sm"
+                            fontSize="sm"
+                            colorScheme="teal"
+                            leftIcon={<Icon as={RiPencilLine} />}
+                          >
+                            Editar
+                          </Button>
+                        </Td>
+                      )}
+                    </Tr>
+                  ))}
+                </Tbody>
+              </Table>
 
-          <Pagination />
+              <Pagination
+                totalCountOfRegisters={data.totalCount}
+                currentPage={page}
+                onPageChange={setPage}
+              />
+            </>
+          )}
         </Box>
       </Flex>
     </Box>
   );
 }
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const { users } = await getUsers(1);
+
+  return {
+    props: {
+      users,
+    },
+  };
+};
